88efde53a39f568ffef8576b6b2d9544ab1ea366
Chie <fubukifei@gmail.com>
2024-10-05 18:46:18 +0800

    chore: init firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    firebase: {
+      gen:2
+    }
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
 })
